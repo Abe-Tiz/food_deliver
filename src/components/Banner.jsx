b4461a934d52e42fd428c77baa5b8ce5e681006a
Abe-Tiz <106752541+Abe-Tiz@users.noreply.github.com>
2024-02-09 09:35:11 +0300

    add navbar component

---
+++ src/components/Banner.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Banner = () => {
+    return (
+        <>
+            <div className="section-container bg-gradient-to-r from-[#FAFAFA] from-0% to-[#FCFCFC] t0-100%">
+                <div className='py-24'>
+                    banner
+                </div>
+            </div> 
+        </>
+    )
+    
+}
+
+export default Banner
